509249e4639815b9c197671ffad7f7b215f228ea
JayHan88 <han5517@kaist.ac.kr>
2020-11-21 17:51:48 +0900

    use set method

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -11,19 +11,13 @@ public class UserDao {
 
 	private ConnectionMaker connectionMaker;
 
-	/*public UserDao() {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		this.connectionMaker = context.getBean("connectionMaker", ConnectionMaker.class);
-	}*/
-
-	/*public UserDao() {
-		DaoFactory daoFactory = new DaoFactory();
-		this.connectionMaker = daoFactory.connectionMaker();
-	}*/
-
-	public UserDao(ConnectionMaker connectionMaker) {
+	public void setConnectionMaker(ConnectionMaker connectionMaker) {
 		this.connectionMaker = connectionMaker;
 	}
+
+	/*public UserDao(ConnectionMaker connectionMaker) {
+		this.connectionMaker = connectionMaker;
+	}*/
 
 	public void add(User user) throws ClassNotFoundException, SQLException {
 
