cad1a0c396bee2fb45ece8f7cbd4a5ed20058a61
JayHan88 <han5517@kaist.ac.kr>
2020-11-25 00:21:01 +0900

    Strategy Pattern

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -16,53 +16,13 @@ public class UserDao {
 		this.dataSource = dataSource;
 	}
 
-	public void add(User user) throws SQLException {
-
-		Connection c = dataSource.getConnection();
-
-		PreparedStatement ps = c.prepareStatement("insert into users (id, name, password) values (?, ?, ?)");
-		ps.setString(1, user.getId());
-		ps.setString(2, user.getName());
-		ps.setString(3, user.getPassword());
-
-		ps.executeUpdate();
-		ps.close();
-		c.close();
-	}
-
-	public User get(String id) throws SQLException {
-
-		Connection c = dataSource.getConnection();
-
-		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-		ps.setString(1, id);
-		ResultSet rs = ps.executeQuery();
-
-		User user = null;
-		if(rs.next()) {
-			user = new User();
-			user.setId(rs.getString("id"));
-			user.setName(rs.getString("name"));
-			user.setPassword(rs.getString("password"));
-		}
-
-		rs.close();
-		ps.close();
-		c.close();
-
-		if (user == null) throw new EmptyResultDataAccessException(1);
-
-		return user;
-	}
-
-	public void deleteAll() throws SQLException {
-
+	public void jdbcContextWithStatementStrategy(StatementStrategy statementStrategy) throws SQLException {
 		Connection c = null;
 		PreparedStatement ps = null;
 
 		try {
 			c = dataSource.getConnection();
-			ps = c.prepareStatement("delete from users");
+			ps = statementStrategy.makePreparedStatement(c);
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			throw e;
@@ -76,6 +36,67 @@ public class UserDao {
 				catch (SQLException e) { }
 			}
 		}
+	}
+
+	public void add(User user) throws SQLException {
+
+		StatementStrategy statementStrategy = new AddStatement(user);
+		jdbcContextWithStatementStrategy(statementStrategy);
+
+	}
+
+	public User get(String id) throws SQLException {
+
+		Connection c = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("select * from users where id = ?");
+			ps.setString(1, id);
+			rs = ps.executeQuery();
+
+			User user = null;
+			if (rs.next()) {
+				user = new User();
+				user.setId(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setPassword(rs.getString("password"));
+			}
+			if (user == null)
+				throw new EmptyResultDataAccessException(1);
+			return user;
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+	}
+
+
+
+	public void deleteAll() throws SQLException {
+		StatementStrategy statementStrategy = new DeleteAllStatement();
+		jdbcContextWithStatementStrategy(statementStrategy);
+
 	}
 
 	public int getCountRow() throws SQLException {
